583baeff8e556b81bc2a8cec661f2b6f92ec7e41
Pablo Javier Duarte Ubeda <pjdu39@gmail.com>
2021-06-15 11:36:02 +0200

    Commit inicial

---
+++ Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JuegoDeLaVida
+{
+    public partial class Form1 : Form
+    {
+        int mapSize = 50;
+        int squareSize = 10;
+        private readonly Size _size;
+
+        public bool[,] rect2dFilledAux { get; set; }
+
+        public MyMatrix matrix { get; set; }
+
+        Pen pen = new Pen(Color.Black);
+        Brush brushTrue = new SolidBrush(Color.Black);
+        Brush brushFalse = new SolidBrush(Color.White);
+
+        public Form1()
+        {
+            InitializeComponent();
+            Width = mapSize * squareSize + 20;
+            Height = mapSize * squareSize + 20;
+
+            _size = new Size(squareSize, squareSize);
+            rect2dFilledAux = new bool[mapSize, mapSize];
+            matrix = new MyMatrix(mapSize, mapSize, _size);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            matrix.Initialize();
+            //matrix.InitializeCustom();
+
+            foreach (var p in matrix.graphicMatrixTrue)
+            {
+                g.FillRectangle(brushTrue, p);
+            }
+
+            while (true)
+            {
+                // Resalta la casilla 10x10 en rojo
+                //g.DrawRectangle(new Pen(Color.Red), new Rectangle(new Point(20 * 10, 20 * 10), new Size(20, 20)));
+
+                for (int i = 0; i < mapSize; i++)
+                {
+                    for (int j = 0; j < mapSize; j++)
+                    {
+                        SetNextCellState(j, i, GiveNeighborsNum(j, i));
+                    }
+                }
+
+                // PRUEBAS
+                for (int i = 0; i < mapSize; i++)
+                {
+                    for (int j = 0; j < mapSize; j++)
+                    {
+                        matrix.stateMatrix[j, i] = rect2dFilledAux[j, i];
+                    }
+                }
+
+                //matrix.stateMatrix = rect2dFilledAux;
+
+                matrix.FillGraphicMatrix();
+
+                foreach(var p in matrix.graphicMatrixTrue)
+                {
+                    g.FillRectangle(brushTrue, p);
+                }
+
+                foreach (var p in matrix.graphicMatrixFalse)
+                {
+                    g.FillRectangle(brushFalse, p);
+                }
+
+                Thread.Sleep(30);
+            }
+        }
+
+
+        public void SetNextCellState(int rectPosX, int rectPosY, int neighbors)
+        {
+            if (matrix.stateMatrix[rectPosX, rectPosY] == true && (neighbors != 3 && neighbors != 2))
+            {
+                rect2dFilledAux[rectPosX, rectPosY] = false;
+            }
+            else if (matrix.stateMatrix[rectPosX, rectPosY] == true && (neighbors == 3 || neighbors == 2))
+            {
+                rect2dFilledAux[rectPosX, rectPosY] = true;
+            }
+            else if (matrix.stateMatrix[rectPosX, rectPosY] == false && neighbors == 3)
+            {
+                rect2dFilledAux[rectPosX, rectPosY] = true;
+            }
+            else if (matrix.stateMatrix[rectPosX, rectPosY] == false && neighbors != 3)
+            {
+                rect2dFilledAux[rectPosX, rectPosY] = false;
+            }
+
+            /*
+            if(rectPosX == 10 && rectPosY == 10)
+            {
+                Console.WriteLine($"Posicion X del rectángulo: {rectPosX}");
+                Console.WriteLine($"Posicion Y del rectángulo: {rectPosY}");
+                Console.WriteLine($"Aux[10, 10]: {rect2dFilledAux[rectPosX, rectPosY]}");
+            }
+            */
+
+            return;
+        }
+
+        public int GiveNeighborsNum(int rectPosX, int rectPosY)
+        {
+            /*
+            if(rectPosX == 10 && rectPosY == 10)
+            {
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.Write((matrix.stateMatrix[9, 9] ? 1 : 0) + " ");
+                Console.Write((matrix.stateMatrix[9, 10] ? 1 : 0) + " ");
+                Console.WriteLine((matrix.stateMatrix[9, 11] ? 1 : 0) + " ");
+                Console.Write((matrix.stateMatrix[10, 9] ? 1 : 0) + " ");
+                Console.Write((matrix.stateMatrix[10, 10] ? 1 : 0) + " ");
+                Console.WriteLine((matrix.stateMatrix[10, 11] ? 1 : 0) + " ");
+                Console.Write((matrix.stateMatrix[11, 9] ? 1 : 0) + " ");
+                Console.Write((matrix.stateMatrix[11, 10] ? 1 : 0) + " ");
+                Console.WriteLine((matrix.stateMatrix[11, 11] ? 1 : 0) + " ");
+                var resultTest = 0;
+
+                int neighborXTest = rectPosX - 1;
+                for (int i = 0; i < 3; i++)
+                {
+                    // Controla los límites del array
+                    if (neighborXTest < 0 || neighborXTest >= mapSize)
+                    {
+                        neighborXTest++;
+                        continue;
+                    }
+
+                    int neighborYTest = rectPosY - 1;
+                    for (int j = 0; j < 3; j++)
+                    {
+                        // Controla los límites del array
+                        if (neighborYTest < 0 || neighborYTest >= mapSize)
+                        {
+                            neighborYTest++;
+                            continue;
+                        }
+
+                        // Cumprueba vecinos pero se excluye a si mismo
+                        if (matrix.stateMatrix[neighborXTest, neighborYTest] == true && (neighborXTest != rectPosX || neighborYTest != rectPosY))
+                        {
+                            resultTest++;
+                        }
+
+                        neighborYTest++;
+                    }
+
+                    neighborXTest++;
+                }
+                Console.WriteLine($"Vecinos en el test: {resultTest}");
+            }
+            */
+
+            var result = 0;
+
+            int neighborX = rectPosX - 1;
+            for (int i=0; i<3; i++)
+            {
+                // Controla los límites del array
+                if (neighborX < 0)
+                {
+                    neighborX += mapSize;
+                }
+                else if (neighborX >= mapSize)
+                {
+                    neighborX -= mapSize;
+                }
+
+                int neighborY = rectPosY - 1;
+                for (int j = 0; j < 3; j++)
+                {
+                    // Controla los límites del array
+                    if (neighborY < 0)
+                    {
+                        neighborY += mapSize;
+                    }
+                    else if (neighborY >= mapSize)
+                    {
+                        neighborY -= mapSize;
+                    }
+
+                    // Cumprueba vecinos pero se excluye a si mismo
+                    if (matrix.stateMatrix[neighborX, neighborY] == true && (neighborX != rectPosX || neighborY != rectPosY))
+                    {
+                        result++;
+                    }
+
+                    neighborY++;
+                }
+
+                neighborX++;
+            }
+
+            /*
+            if (rectPosX == 10 && rectPosY == 10)
+            {
+                Console.WriteLine($"Vecinos calculados: {result}");
+            }
+            */
+
+            return result;
+        }
+
+        public Rectangle[] ConvierteArrayRectagulos2dA1d(Rectangle[,] rects)
+        {
+            var result = new Rectangle[50*50];
+
+            for(int i=0; i<50; i++)
+            {
+                for (int j = 0; j < 50; j++)
+                {
+                    // Calcula la posición que le corresponde en el array de una dimensión
+                    result[j + i * 50] = rects[i, j];
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ MyMatrix.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuegoDeLaVida
+{
+    public class MyMatrix
+    {
+        private readonly int _dimX;
+        private readonly int _dimY;
+        private readonly Size _rectSize;
+
+        public bool[,] stateMatrix { get; set; }
+        public Rectangle[,] graphicMatrixTrue { get; set; }
+        public Rectangle[,] graphicMatrixFalse { get; set; }
+
+        public MyMatrix(int dimX, int dimY, Size rectSize)
+        {
+            _dimX = dimX;
+            _dimY = dimY;
+            _rectSize = rectSize;
+
+            stateMatrix = new bool[dimX, dimY];
+            graphicMatrixTrue = new Rectangle[dimX, dimY];
+            graphicMatrixFalse = new Rectangle[dimX, dimY];
+        }
+
+        public void FillGraphicMatrix()
+        {
+            int pointX;
+            int pointY;
+
+            graphicMatrixTrue = new Rectangle[_dimX, _dimY];
+            graphicMatrixFalse = new Rectangle[_dimX, _dimY];
+
+            for (int i=0; i<_dimX; i++)
+            {
+                for (int j = 0; j < _dimY; j++)
+                {
+                    pointX = i * _rectSize.Width;
+                    pointY = j * _rectSize.Height;
+
+                    var point = new Point(pointX, pointY);
+                    var rect = new Rectangle(point, _rectSize);
+
+                    if (stateMatrix[j, i]) graphicMatrixTrue[j, i] = rect;
+                    if (!stateMatrix[j, i]) graphicMatrixFalse[j, i] = rect;
+
+                    /*
+                    if(i == 10 && j == 10)
+                    {
+                        Console.WriteLine("ESTADO DE LA MATRIZ EN EL METODO QUE ACTUALIZA GRAFICOS");
+                        Console.WriteLine();
+                        Console.Write((stateMatrix[9, 9] ? 1 : 0) + " ");
+                        Console.Write((stateMatrix[9, 10] ? 1 : 0) + " ");
+                        Console.WriteLine((stateMatrix[9, 11] ? 1 : 0) + " ");
+                        Console.Write((stateMatrix[10, 9] ? 1 : 0) + " ");
+                        Console.Write((stateMatrix[10, 10] ? 1 : 0) + " ");
+                        Console.WriteLine((stateMatrix[10, 11] ? 1 : 0) + " ");
+                        Console.Write((stateMatrix[11, 9] ? 1 : 0) + " ");
+                        Console.Write((stateMatrix[11, 10] ? 1 : 0) + " ");
+                        Console.WriteLine((stateMatrix[11, 11] ? 1 : 0) + " ");
+
+                        var rectTest = new Rectangle(new Point(0, 0), _rectSize);
+
+                        Console.WriteLine();
+                        Console.Write((graphicMatrixTrue[9, 9].Location != rectTest.Location ? 1 : 0) + " ");
+                        Console.Write((graphicMatrixTrue[9, 10].Location != rectTest.Location ? 1 : 0) + " ");
+                        Console.WriteLine((graphicMatrixTrue[9, 11].Location != rectTest.Location ? 1 : 0) + " ");
+                        Console.Write((graphicMatrixTrue[10, 9].Location != rectTest.Location ? 1 : 0) + " ");
+                        Console.Write((graphicMatrixTrue[10, 10].Location != rectTest.Location ? 1 : 0) + " ");
+                        Console.WriteLine((graphicMatrixTrue[10, 11].Location != rectTest.Location ? 1 : 0) + " ");
+                        Console.Write((graphicMatrixTrue[11, 9].Location != rectTest.Location ? 1 : 0) + " ");
+                        Console.Write((graphicMatrixTrue[11, 10].Location != rectTest.Location ? 1 : 0) + " ");
+                        Console.WriteLine((graphicMatrixTrue[11, 11].Location != rectTest.Location ? 1 : 0) + " ");
+                    }
+                    */
+                }
+            }
+        }
+
+        public void RandomFillState()
+        {
+            Random r = new Random();
+            for (int i = 0; i < _dimX; i++)
+            {
+                for (int j = 0; j < _dimX; j++)
+                {
+                    int num = r.Next(0, 2);
+                    stateMatrix[i, j] = Convert.ToBoolean(num);
+                }
+            }
+        }
+
+        public void CreatePlane()
+        {
+            stateMatrix = new bool[_dimX, _dimY];
+            stateMatrix[9, 10] = true;
+            stateMatrix[10, 11] = true;
+            stateMatrix[11, 9] = true;
+            stateMatrix[11, 10] = true;
+            stateMatrix[11, 11] = true;
+        }
+
+        public void Initialize()
+        {
+            RandomFillState();
+            FillGraphicMatrix();
+        }
+
+        public void InitializeCustom()
+        {
+            CreatePlane();
+            FillGraphicMatrix();
+        }
+    }
+}
